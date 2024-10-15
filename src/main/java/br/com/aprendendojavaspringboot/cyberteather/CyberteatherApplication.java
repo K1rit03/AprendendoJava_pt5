d0f54a1afb0a8cfa214a8a1250495f8129444ad7
Thiago <thiagodeboliveira@outlook.com>
2024-10-14 23:45:38 -0300

    Iniciando 2 formação de java com foco em spring boot

---
+++ src/main/java/br/com/aprendendojavaspringboot/cyberteather/CyberteatherApplication.java
@@ -0,0 +1,13 @@
+package br.com.aprendendojavaspringboot.cyberteather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberteatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberteatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aprendendojavaspringboot/cyberteather/CyberteatherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aprendendojavaspringboot.cyberteather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberteatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
